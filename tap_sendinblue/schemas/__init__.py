e9ea5a9c6916e4a1293053cceb840d25644b9416
Fernandesfelipe1989 <fernandes.felipe1989@gmail.com>
2023-03-22 20:04:49 -0300

    WIP: Update the schemas.

--- tap_sendinblue/schemas/__init__.py
+++ tap_sendinblue/schemas/__init__.py
@@ -1,3 +1,5 @@
 from tap_sendinblue.schemas.campaigns import schema as campaigns
 from tap_sendinblue.schemas.listmembers import schema as list_members
 from tap_sendinblue.schemas.lists import schema as lists
+from tap_sendinblue.schemas.smtp_report import schema as smtp_report
+from tap_sendinblue.schemas.smtp_events import schema as smtp_events
